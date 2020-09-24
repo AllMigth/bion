4c4628c8c4debd4c374edfc9d0fa3af964cf9a59
Juan <jgarzon292@gmail.com>
2020-09-24 01:43:41 -0500

    splash and login

--- app/src/main/java/com/simulador/bin/tareas/Login.java
+++ app/src/main/java/com/simulador/bin/tareas/Login.java
@@ -2,15 +2,41 @@ package com.simulador.bin.tareas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Login extends AppCompatActivity {
+    Button callSignUp;
+    ImageView imageView;
+    TextView LogoText, sloganText;
+    TextInputLayout username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        //Hooks
+        callSignUp = findViewById(R.id.nuevoUsuario);
+        imageView = findViewById(R.id.logo_image);
+        LogoText = findViewById(R.id.subText);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        callSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/simulador/bin/tareas/MainActivity.java
+++ app/src/main/java/com/simulador/bin/tareas/MainActivity.java
@@ -2,9 +2,12 @@ package com.simulador.bin.tareas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -44,8 +47,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent = new Intent(MainActivity.this, Login.class);
-                startActivity(intent);
-                finish();
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View,String>(imageView, "logo_image");
+                pairs[1] = new Pair<View,String>(logo, "logo_text");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                startActivity(intent, options.toBundle());
             }
         }, SPLASH_SCREEN);
 
